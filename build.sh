9e8b028905ad8a5aa557801c7a408be0cd461543
= <=>
2026-02-13 00:01:35 +0530

    move requirements to root

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-pip install -r theboxcoders/requirements.txt
+pip install -r requirements.txt
 python theboxcoders/manage.py collectstatic --noinput
 python theboxcoders/manage.py migrate
